c1f9cd77abc34abfea48a46fd09b8707b9006981
Murilo-MRS <murilosantana1993@gmail.com>
2022-08-10 10:36:23 -0300

    requisito 4

--- helpers/fetchItem.js
+++ helpers/fetchItem.js
@@ -1,13 +1,12 @@
-const fetchItem = async () => {
-  // seu código aqui
-  // const url = `https://api.mercadolibre.com/sites/MLB/search?q=${QUERY}`;
-  // try {
-  //   const response = await fetch(url);
-  //   const data = await response.json();
-  //   return data;
-  // } catch (error) {
-  //   return error;
-  // }
+const fetchItem = async (ItemID) => {
+  const url = `https://api.mercadolibre.com/items/${ItemID}`;
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    return error;
+  }
 };
 
 if (typeof module !== 'undefined') {
